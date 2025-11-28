61136061c0d99b7d2a9f18911e93e234b9b12c15
huguesblessy-lgtm <huguesblessy@gmail.com>
2025-11-28 09:37:36 +0200

    Fixed corsOption.js filename and backend config

--- backend/middleware/cors.Option.js
+++
@@ -1,9 +0,0 @@
-const corsOptions = {
-    origin: process.env.NODE_ENV === 'production' ? process.env.ALLOW_CORS_URL : 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  };
-
-
-module.exports = corsOptions
---
+++ backend/middleware/corsOptions.js
@@ -0,0 +1,11 @@
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? process.env.ALLOW_CORS_URL
+      : 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+module.exports = corsOptions;
